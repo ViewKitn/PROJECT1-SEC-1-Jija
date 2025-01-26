8893d732aaad279598acbaa715702803560c701d
unknown <naruedoom.srir@mail.kmutt.ac.th>
2025-01-26 17:02:00 +0700

    051 [add] sturcture of project into repositories

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
   <div class="game-container">
     <section class="home-page">
       <!-- code here -->
-       
     </section>
     <section class="category-page">
       <!-- code here -->
-
     </section>
     <section class="playgame-page">
       <!-- code here -->
-
     </section>
     <section class="score-page">
       <!-- code here -->
-
     </section>
   </div>
 </template>
