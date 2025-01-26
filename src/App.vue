8fbcd1c672bd8bb79cf6be0b1e75b6855906c400
Naruedom-sri <naruedoom.srir@mail.kmutt.ac.th>
2025-01-26 21:04:57 +0700

    051 [add] feature random choice

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,102 @@
 <script setup>
-
+const animalList = [
+  "monkey",
+  "shark",
+  "bear",
+  "camel",
+  "donkey",
+  "rabbit",
+  "zebra",
+  "dog",
+  "cat",
+  "deer",
+  "squirrel",
+  "fox",
+  "pig",
+  "horse",
+  "cheetah",
+  "elephant",
+  "giraffe",
+  "cow",
+  "goat",
+  "butterfly",
+];
+const fruitList = [
+  "apple",
+  "banana",
+  "blueberry",
+  "cherry",
+  "cantaloupe",
+  "grape",
+  "durian",
+  "kiwi",
+  "lemon",
+  "lime",
+  "mangosteen",
+  "mango",
+  "lychee",
+  "orange",
+  "pineapple",
+  "rambutan",
+  "starfruit",
+  "strawberry",
+  "watermelon",
+  "papaya",
+];
+const countryList = [
+  "argentina",
+  "australia",
+  "brazil",
+  "cambodia",
+  "china",
+  "america",
+  "egypt",
+  "finland",
+  "france",
+  "georgia",
+  "germany",
+  "iceland",
+  "india",
+  "indonesia",
+  "italy",
+  "japan",
+  "mexico",
+  "philippines",
+  "russia",
+  "portugal",
+];
+const homeEquipmentList = [
+  "refrigerator",
+  "microwave",
+  "dishwasher",
+  "washing machine",
+  "clothes iron",
+  "hair dyer",
+  "air conditioner",
+  "heater",
+  "electric fan",
+  "coffee maker",
+  "blender",
+  "rice cooker",
+  "oven",
+  "television",
+  "clock",
+  "computer",
+  "lamp",
+  "speaker",
+  "griddle",
+  "toothbrush",
+];
+const choiceList = [];
+const randomChoice = (arr) => {
+  for (let i = 0; i < 5; i++) {
+    let index = Math.floor(Math.random() * arr.length);
+    choiceList.push(arr[index]);
+    arr.splice(index, 1);
+  }
+};
+randomChoice(animalList);
+console.log(choiceList);
 </script>
 
 <template>
