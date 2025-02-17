f9e656cdde9741959a3b7bf59ab8923fa83a1723
Naruedom-sri <naruedoom.srir@mail.kmutt.ac.th>
2025-02-17 19:01:59 +0700

    051 [delete] unused code

--- src/App.vue
+++ src/App.vue
@@ -88,13 +88,7 @@ const homeEquipmentList = [
   "griddle",
   "toothbrush",
 ];
-const complimentList = [
-  "Bravo",
-  "Nice",
-  "Great",
-  "Awesome",
-  "Fabulous",
-];
+const complimentList = ["Bravo", "Nice", "Great", "Awesome", "Fabulous"];
 
 // variable
 const choiceList = ref([]);
@@ -111,7 +105,7 @@ let time = ref(5);
 let setintervalTimerId;
 const musicPlayer = ref("starting");
 const onMusic = ref(false);
-let complimentText = ref('');  
+let complimentText = ref("");
 
 // feature audio
 const playMusic = () => {
@@ -120,7 +114,7 @@ const playMusic = () => {
   else musicPlayer.value.pause();
 };
 const playClickSound = () => {
-  const audio = new Audio('../Audio/OnClick-1.wav');
+  const audio = new Audio("../audio/OnClick-1.wav");
   audio.play();
 };
 //feature timer
@@ -187,7 +181,8 @@ const resetChoiceList = () => {
 // feature answer
 
 const showCompliment = () => {
-  const randomCompliment = complimentList[Math.floor(Math.random() * complimentList.length)];
+  const randomCompliment =
+    complimentList[Math.floor(Math.random() * complimentList.length)];
   complimentText.value = randomCompliment;
   return randomCompliment;
 };
@@ -207,7 +202,7 @@ const checkAnswer = (userSelect) => {
   if (answer.value === choiceList.value[userSelect]) {
     addScore();
     stateAnswer.value = "correct";
-    const compliment = showCompliment();  
+    const compliment = showCompliment();
     console.log(compliment);
   } else {
     stateAnswer.value = "incorrect";
@@ -306,77 +301,76 @@ const showmodal = () => {
 
 <template>
   <div class="game-container">
-    <!-- home page -->
-   
     <section
       class="home-page h-screen flex flex-col place-items: center justify-center bg-linear-to-r from-fuchsia-600 to-blue-800"
       v-show="page === 'home'"
     >
-    <video autoplay muted loop class="background-video-first absolute inset-0 w-full h-full object-cover mix-blend-screen">
-      <source src="./assets/video/Sequence1.webm" type="video/webm" />
-    </video>
-      <!-- code here -->
-      
-        <div class="btn-audio absolute top-4 right-7">
-            <label class="swap bg-red-600 h-25 w-25 rounded-full">
-              <input type="checkbox" @click="playMusic" v-model="onMusic" />
-              <svg
-                class="swap-on fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="70"
-                height="70"
-                viewBox="0 0 24 24"
-                style="color: white"
-              >
-                <path
-                  d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 
-              21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"
-                />
-              </svg>
+      <video
+        autoplay
+        muted
+        loop
+        class="background-video-first absolute inset-0 w-full h-full object-cover mix-blend-screen opacity-30"
+      >
+        <source src="./assets/video/Sequence1.webm" type="video/webm" />
+      </video>
 
-              <svg
-                class="swap-off fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="70"
-                height="70"
-                viewBox="0 0 24 24"
-                style="color: white"
-              >
-                <path
-                  d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,
+      <div class="btn-audio absolute top-4 right-7">
+        <label class="swap bg-red-600 h-25 w-25 rounded-full">
+          <input type="checkbox" @click="playMusic" v-model="onMusic" />
+          <svg
+            class="swap-on fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="70"
+            height="70"
+            viewBox="0 0 24 24"
+            style="color: white"
+          >
+            <path
+              d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 
+              21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"
+            />
+          </svg>
+
+          <svg
+            class="swap-off fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="70"
+            height="70"
+            viewBox="0 0 24 24"
+            style="color: white"
+          >
+            <path
+              d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,
               14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z"
-                />
-              </svg>
-            </label>
-            <audio controls class="hidden" ref="musicPlayer" loop>
-              <source src="../Audio/GameMusic.mp3" type="audio/mp3" />
-              <p>
-                Your browser doesn't support this audio file. Here is a
-                <a href="../Audio/GameMusic.mp3">link to the audio</a>
-                instead.
-              </p>
-            </audio>
-          </div>
-        <div class="text-center bg-black rounded-lg h-130"> 
+            />
+          </svg>
+        </label>
+        <audio controls class="hidden" ref="musicPlayer" loop>
+          <source src="../audio/GameMusic.mp3" type="audio/mp3" />
+          <p>
+            Your browser doesn't support this audio file. Here is a
+            <a href="../audio/GameMusic.mp3">link to the audio</a>
+            instead.
+          </p>
+        </audio>
+      </div>
+      <div class="text-center bg-black rounded-lg h-130">
         <h1 class="text-8xl font-bold pt-21 mb-6 drop-shadow-lg text-white">
           Welcome to the Game!
         </h1>
-        <p class=" text-3xl mb-10 opacity-90 text-white">
+        <p class="text-3xl mb-10 opacity-90 text-white">
           What is it? Let's take a guess !?😘
         </p>
         <button
-          class="outline  justify-center text-center [transition:_all_.3s_ease] leading-tight bg-white hover:bg-yellow-400 hover:text-white hover:ring-white hover:ring-3 transition-all w-auto rounded-full px-8 md:px-12 h-28 text-6xl  text-purple-500 font-bold mb-6 drop-shadow-lg uppercase hover:cursor-pointer"
-          @click="page = 'category',playClickSound()"
+          class="outline justify-center text-center [transition:_all_.3s_ease] leading-tight bg-white hover:bg-yellow-400 hover:text-white hover:ring-white hover:ring-3 transition-all w-auto rounded-full px-8 md:px-12 h-28 text-6xl text-purple-500 font-bold mb-6 drop-shadow-lg uppercase hover:cursor-pointer"
+          @click="(page = 'category'), playClickSound()"
         >
           Play
         </button>
       </div>
     </section>
 
-    <!-- category page -->
     <section class="category-page" v-show="page === 'category'">
-      <!-- code here -->
-      
       <div
         id="app"
         class="flex flex-col items-center min-h-screen p-4 bg-linear-to-r from-fuchsia-500 to-blue-600"
@@ -384,57 +378,94 @@ const showmodal = () => {
         <h1 class="text-8xl font-bold text-white shadow-lg pt-15 mb-15">
           Category
         </h1>
-        
-        <div id="category-content" class="mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full">
-          <video autoplay muted loop
+
+        <div
+          id="category-content"
+          class="mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full"
+        >
+          <video
+            autoplay
+            muted
+            loop
             class="background-video-second absolute inset-0 w-full h-full object-cover mix-blend-screen"
           >
             <source src="./assets/video/Sequence2.webm" type="video/webm" />
           </video>
-          <div id="animals-content" class="category-item p-4 bg-yellow-300 rounded-lg">
+          <div
+            id="animals-content"
+            class="category-item p-4 bg-yellow-300 rounded-lg"
+          >
             <h3 class="text-4xl font-bold text-gray-800 mb-4">Animals</h3>
             <img
               src="./assets/imgs/category/animal.jpg"
               alt="Animals"
               class="w-[100%] h-100 object-cover rounded-4xl mb-4 hover:scale-110"
-              @click="(category = 'animal'), gameStart(), (page = 'play'), playClickSound()"
+              @click="
+                (category = 'animal'),
+                  gameStart(),
+                  (page = 'play'),
+                  playClickSound()
+              "
             />
           </div>
 
-          <div id="foods-content" class="category-item p-4  bg-green-400 rounded-lg">
+          <div
+            id="foods-content"
+            class="category-item p-4 bg-green-400 rounded-lg"
+          >
             <h3 class="text-4xl font-bold text-gray-800 mb-4">Fruits</h3>
             <img
               src="./assets/imgs/category/fruit.png"
               alt="Fruits"
               class="w-[100%] h-100 object-cover rounded-4xl mb-4 hover:scale-110"
-              @click="(category = 'fruit'), gameStart(), (page = 'play'), playClickSound()"
-              
+              @click="
+                (category = 'fruit'),
+                  gameStart(),
+                  (page = 'play'),
+                  playClickSound()
+              "
             />
           </div>
 
-          <div id="objects-content" class="category-item p-4  bg-blue-300 rounded-lg">
+          <div
+            id="objects-content"
+            class="category-item p-4 bg-blue-300 rounded-lg"
+          >
             <h3 class="text-4xl font-bold text-gray-800 mb-4">Equipments</h3>
             <img
               src="./assets/imgs/category/equipment.jpg"
               alt="Objects"
               class="w-[100%] h-100 object-cover rounded-4xl mb-4 hover:scale-110"
-              @click="(category = 'equipment'), gameStart(), (page = 'play'), playClickSound()"
+              @click="
+                (category = 'equipment'),
+                  gameStart(),
+                  (page = 'play'),
+                  playClickSound()
+              "
             />
           </div>
-          <div id="places-content" class="category-item p-4  bg-orange-400 rounded-lg">
+          <div
+            id="places-content"
+            class="category-item p-4 bg-orange-400 rounded-lg"
+          >
             <h3 class="text-4xl font-bold text-gray-800 mb-4">Countries</h3>
             <img
               src="./assets/imgs/category/country.png"
               alt="Places"
               class="w-[100%] h-100 object-cover rounded-4xl mb-4 hover:scale-110"
-              @click="(category = 'country'), gameStart(), (page = 'play'), playClickSound()"
+              @click="
+                (category = 'country'),
+                  gameStart(),
+                  (page = 'play'),
+                  playClickSound()
+              "
             />
           </div>
         </div>
         <div class="flex space-x-1 items-center">
           <div class="btn-back">
             <button
-              @click="page = 'home',playClickSound()"
+              @click="(page = 'home'), playClickSound()"
               class="mt-15 px-8 md:px-12 h-28 rounded-full text-6xl text-purple-700 bg-white duration-200 ease-in hover:cursor-pointer hover:bg-red-500 hover:text-white hover:font-medium"
             >
               Back
@@ -444,13 +475,10 @@ const showmodal = () => {
       </div>
     </section>
 
-    <!-- play game page -->
     <section v-show="page === 'play'" class="playgame-page">
-      <!-- code here -->
       <div
         class="playgame-container w-full bg-linear-to-r from-purple-500 to-pink-600"
       >
-        <!-- show-modal -->
         <div
           v-show="modal === 'show'"
           class="show-modal w-full h-screen flex justify-center fixed z-10 bg-black/50"
@@ -460,7 +488,11 @@ const showmodal = () => {
               <h1 class="pt-8 w-fit mx-auto text-8xl text-green-600">
                 {{ stateAnswer === "correct" ? "Correct" : "Incorrect" }}
               </h1>
-              <p v-show="stateAnswer === 'correct'" class="w-fit mx-auto my-4 text-2xl text-red-400">{{ complimentText }}
+              <p
+                v-show="stateAnswer === 'correct'"
+                class="w-fit mx-auto my-4 text-2xl text-red-400"
+              >
+                {{ complimentText }}
               </p>
               <p
                 v-show="stateAnswer === 'incorrect'"
@@ -470,8 +502,8 @@ const showmodal = () => {
               </p>
               <div class="btn-next flex justify-center">
                 <button
-                  @click="nextRound(),playClickSound()"
-                  class="h-20 w-40 my-3 py-2 rounded-4xl text-2xl text-black bg-yellow-300 duration-200 ease-in hover:scale-110"
+                  @click="nextRound(), playClickSound()"
+                  class="h-20 w-40 my-3 py-2 rounded-4xl text-2xl text-black bg-yellow-300 duration-200 ease-in hover:scale-110 hover:cursor-pointer"
                 >
                   Next
                 </button>
@@ -479,19 +511,20 @@ const showmodal = () => {
             </div>
           </div>
         </div>
-        <!-- content-game -->
         <div class="game-content w-full h-screen">
           <header class="flex h-24">
             <div class="btn-back flex-1 self-center">
               <button
-                @click="clearGame(), (page = 'category'),playClickSound()"
-                class="w-40 mx-6 py-3 bg- rounded-4xl text-2xl text-black bg-zinc-100/70 duration-200 ease-in hover:cursor-pointer hover:bg-red-500 hover:text-white hover:font-medium"
+                @click="clearGame(), (page = 'category'), playClickSound()"
+                class="w-40 mx-6 py-3 rounded-4xl text-2xl text-purple-700 bg-white duration-200 ease-in hover:cursor-pointer hover:bg-red-500 hover:text-white hover:font-medium"
               >
                 Back
               </button>
             </div>
             <div class="timer self-center">
-              <h1 class="text-4xl text-yellow-400 mt-15 drop-shadow-lg font-bold">{{ time }}</h1>
+              <h1 class="text-4xl text-yellow-400 drop-shadow-lg font-bold">
+                {{ time }}
+              </h1>
             </div>
             <div class="round flex-1 self-center">
               <h1
@@ -516,7 +549,10 @@ const showmodal = () => {
               v-for="(choice, index) in choiceList"
               :key="index"
               @click="
-                stopTimer(setintervalTimerId), checkAnswer(index), showmodal(),playClickSound()
+                stopTimer(setintervalTimerId),
+                  checkAnswer(index),
+                  showmodal(),
+                  playClickSound()
               "
               class="w-70 h-30 mx-5 py-4 rounded-4xl bg-zinc-100/70 text-5xl text-black duration-200 ease-in hover:scale-125 hover:text-white hover:cursor-pointer hover:font-medium"
               :class="getColorButton(index)"
@@ -528,7 +564,6 @@ const showmodal = () => {
       </div>
     </section>
 
-    <!-- score page -->
     <section
       class="score-page h-screen bg-linear-to-r from-fuchsia-600 to-blue-800 flex justify-center items-center border-30 rounded-lg"
       v-show="page === 'score'"
@@ -547,14 +582,14 @@ const showmodal = () => {
         </div>
         <div class="mt-35 flex justify-center gap-30">
           <button
-            @click="clearGame(), (page = 'home'),playClickSound()"
-            class="outline  justify-center text-center [transition:_all_.3s_ease] leading-tight bg-white hover:bg-yellow-400 hover:text-white hover:ring-white hover:ring-3 transition-all w-auto rounded-full px-8 md:px-12 h-25 text-6xl  text-purple-500 mb-6 drop-shadow-lg uppercase hover:cursor-pointer"
+            @click="clearGame(), (page = 'home'), playClickSound()"
+            class="outline justify-center text-center [transition:_all_.3s_ease] leading-tight bg-white hover:bg-yellow-400 hover:text-white hover:ring-white hover:ring-3 transition-all w-auto rounded-full px-8 md:px-12 h-25 text-6xl text-purple-500 mb-6 drop-shadow-lg uppercase hover:cursor-pointer"
           >
             HOME
           </button>
           <button
-            @click="clearGame(), gameStart(), (page = 'play'),playClickSound()"
-            class="outline justify-center text-center [transition:_all_.3s_ease] leading-tight bg-white hover:bg-green-400 hover:text-white hover:ring-white hover:ring-3 transition-all w-auto rounded-full px-8 md:px-12 h-25 text-6xl  text-purple-500 mb-6 drop-shadow-lg uppercase hover:cursor-pointer"
+            @click="clearGame(), gameStart(), (page = 'play'), playClickSound()"
+            class="outline justify-center text-center [transition:_all_.3s_ease] leading-tight bg-white hover:bg-green-400 hover:text-white hover:ring-white hover:ring-3 transition-all w-auto rounded-full px-8 md:px-12 h-25 text-6xl text-purple-500 mb-6 drop-shadow-lg uppercase hover:cursor-pointer"
           >
             PLAY AGAIN
           </button>
@@ -572,10 +607,11 @@ const showmodal = () => {
   </div>
 </template>
 
+
 <style scoped>
-.background-video-first ,
-.background-video-second ,
-.background-video-last{
+.background-video-first,
+.background-video-second,
+.background-video-last {
   pointer-events: none;
 }
 .modal-slide {
